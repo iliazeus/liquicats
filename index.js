4d109dcee1d77fb1ba1a5f36067c3205b0a0c1fe
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2020-06-27 17:25:38 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,243 @@
+var Direction;
+(function (Direction) {
+    Direction[Direction["Right"] = 0] = "Right";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Up"] = 3] = "Up";
+})(Direction || (Direction = {}));
+var BackgroundType;
+(function (BackgroundType) {
+    BackgroundType[BackgroundType["Floor"] = 0] = "Floor";
+    BackgroundType[BackgroundType["Exit"] = 1] = "Exit";
+})(BackgroundType || (BackgroundType = {}));
+var ForegroundType;
+(function (ForegroundType) {
+    ForegroundType[ForegroundType["Empty"] = 0] = "Empty";
+    ForegroundType[ForegroundType["Wall"] = 1] = "Wall";
+    ForegroundType[ForegroundType["CatHead"] = 2] = "CatHead";
+    ForegroundType[ForegroundType["CatBody"] = 3] = "CatBody";
+    ForegroundType[ForegroundType["CatTail"] = 4] = "CatTail";
+})(ForegroundType || (ForegroundType = {}));
+var Tile = /** @class */ (function () {
+    function Tile(backgroundType, backgroundId, foregroundType, foregroundId, headDirection, tailDirection) {
+        this.backgroundType = backgroundType;
+        this.backgroundId = backgroundId;
+        this.foregroundType = foregroundType;
+        this.foregroundId = foregroundId;
+        this.headDirection = headDirection;
+        this.tailDirection = tailDirection;
+    }
+    Tile.prototype.encode = function () {
+        return (this.backgroundType << 0)
+            | (this.backgroundId << 4)
+            | (this.foregroundType << 8)
+            | (this.foregroundId << 12)
+            | (this.headDirection << 16)
+            | (this.tailDirection << 20);
+    };
+    Tile.decode = function (n) {
+        return new Tile((n >> 0) & 0xf, (n >> 4) & 0xf, (n >> 8) & 0xf, (n >> 12) & 0xf, (n >> 16) & 0xf, (n >> 20) & 0xf);
+    };
+    Tile.prototype.hasCat = function () {
+        return this.foregroundType === ForegroundType.CatHead
+            || this.foregroundType === ForegroundType.CatBody
+            || this.foregroundType === ForegroundType.CatTail;
+    };
+    return Tile;
+}());
+var Field = /** @class */ (function () {
+    function Field(width, height, tiles) {
+        this.width = width;
+        this.height = height;
+        this.tiles = tiles;
+        if (tiles.length !== height) {
+            throw new Error('invalid height');
+        }
+        if (tiles.some(function (row) { return row.length !== width; })) {
+            throw new Error('invalid width');
+        }
+    }
+    Field.prototype.encode = function () {
+        var arr = new Uint32Array(this.height * this.width + 2);
+        arr[arr.length - 2] = this.width;
+        arr[arr.length - 1] = this.height;
+        for (var row = 0; row < this.height; row += 1) {
+            for (var col = 0; col < this.width; col += 1) {
+                arr[row * this.width + col] = this.tiles[row][col].encode();
+            }
+        }
+        return arr;
+    };
+    Field.decode = function (arr) {
+        var width = arr[arr.length - 2];
+        var height = arr[arr.length - 1];
+        if (arr.length !== width * height + 2) {
+            throw new Error('invalid field');
+        }
+        var tiles = Array();
+        for (var row = 0; row < height; row += 1) {
+            var rowTiles = Array();
+            for (var col = 0; col < width; col += 1) {
+                rowTiles.push(Tile.decode(arr[row * width + col]));
+            }
+            tiles.push(rowTiles);
+        }
+        return new Field(width, height, tiles);
+    };
+    return Field;
+}());
+var Game = /** @class */ (function () {
+    function Game(field) {
+        this.field = field;
+    }
+    Game.prototype.isWon = function () {
+        return this.field.tiles.every(function (rowTiles) { return rowTiles.every(function (tile) { return tile.hasCat()
+            && tile.foregroundId === 1
+            && tile.backgroundType === BackgroundType.Exit; }); });
+    };
+    Game.prototype.moveCat = function (fromRow, fromCol, toRow, toCol) {
+        var fromTile = this.field.tiles[fromRow][fromCol];
+        var toTile = this.field.tiles[toRow][toCol];
+        if (fromTile.foregroundType !== ForegroundType.CatHead
+            && fromTile.foregroundType !== ForegroundType.CatTail) {
+            return;
+        }
+        if (toTile.foregroundType !== ForegroundType.Empty) {
+            return;
+        }
+        var dist = Math.abs(toRow - fromRow) + Math.abs(toCol - fromCol);
+        if (dist !== 1) {
+            return;
+        }
+        var otherEndForegroundType = fromTile.foregroundType === ForegroundType.CatHead
+            ? ForegroundType.CatTail
+            : ForegroundType.CatHead;
+        var frontDirection = fromTile.foregroundType === ForegroundType.CatHead
+            ? "headDirection"
+            : "tailDirection";
+        var backDirection = fromTile.foregroundType === ForegroundType.CatHead
+            ? "tailDirection"
+            : "headDirection";
+        var lastTile = toTile;
+        var lastRow = toRow;
+        var lastCol = toCol;
+        while (lastTile.foregroundType !== otherEndForegroundType) {
+            toTile.foregroundId = fromTile.foregroundId;
+            toTile.foregroundType = fromTile.foregroundType;
+            if (lastRow - toRow === -1)
+                toTile[frontDirection] = Direction.Up;
+            if (lastRow - toRow === +1)
+                toTile[frontDirection] = Direction.Down;
+            if (lastCol - toCol === -1)
+                toTile[frontDirection] = Direction.Left;
+            if (lastCol - toCol === +1)
+                toTile[frontDirection] = Direction.Right;
+            if (fromRow - toRow === -1)
+                toTile[backDirection] = Direction.Up;
+            if (fromRow - toRow === +1)
+                toTile[backDirection] = Direction.Down;
+            if (fromCol - toCol === -1)
+                toTile[backDirection] = Direction.Left;
+            if (fromCol - toCol === +1)
+                toTile[backDirection] = Direction.Right;
+            fromTile.foregroundId = 0;
+            fromTile.foregroundType = ForegroundType.Empty;
+            lastTile = toTile;
+            lastRow = toRow;
+            lastCol = toCol;
+            toTile = fromTile;
+            toRow = fromRow;
+            toCol = fromCol;
+            if (fromTile[backDirection] === Direction.Up)
+                fromRow -= 1;
+            if (fromTile[backDirection] === Direction.Down)
+                fromRow += 1;
+            if (fromTile[backDirection] === Direction.Left)
+                fromCol -= 1;
+            if (fromTile[backDirection] === Direction.Right)
+                fromCol += 1;
+            fromTile = this.field.tiles[fromRow][fromCol];
+        }
+    };
+    return Game;
+}());
+function setBackgroundTileData(el, tile) {
+    el.setAttribute("data-backgroundType", String(tile.backgroundType));
+    el.setAttribute("data-backgroundId", String(tile.backgroundId));
+}
+function setForegroundTileData(el, tile) {
+    el.setAttribute("data-foregroundType", String(tile.foregroundType));
+    el.setAttribute("data-foregroundId", String(tile.foregroundId));
+    el.setAttribute("data-headDirection", String(tile.headDirection));
+    el.setAttribute("data-tailDirection", String(tile.tailDirection));
+}
+function updateTileData($background, $foreground, game) {
+    $background.querySelectorAll(".tile").forEach(function ($tile) {
+        var row = Number($tile.getAttribute("data-row"));
+        var col = Number($tile.getAttribute("data-col"));
+        setBackgroundTileData($tile, game.field.tiles[row][col]);
+    });
+    $foreground.querySelectorAll(".tile").forEach(function ($tile) {
+        var row = Number($tile.getAttribute("data-row"));
+        var col = Number($tile.getAttribute("data-col"));
+        setForegroundTileData($tile, game.field.tiles[row][col]);
+    });
+}
+// const fieldData = Uint32Array.of(
+//     0x000100, 0x000100, 0x000100, 0x000100, 0x000100,
+//     0x000100, 0x131200, 0x312400, 0x000000, 0x000100,
+//     0x000100, 0x131300, 0x312300, 0x000000, 0x000100,
+//     0x000100, 0x131400, 0x312200, 0x000000, 0x000100,
+//     0x000100, 0x000100, 0x000001, 0x000100, 0x000100,
+//     5, 5
+// );
+var fieldData = Uint32Array.from(window.location.hash.slice(1).split(',').map(function (s) { return Number(s); }));
+console.log(fieldData.toString());
+var game = new Game(Field.decode(fieldData));
+var fromRow = -1;
+var fromCol = -1;
+var toRow = -1;
+var toCol = -1;
+window.addEventListener("load", function () {
+    var $foreground = document.getElementById("foreground");
+    var $background = document.getElementById("background");
+    var _loop_1 = function (row) {
+        var $foregroundRow = document.createElement("tr");
+        var $backgroundRow = document.createElement("tr");
+        var _loop_2 = function (col) {
+            var $foregroundTile = document.createElement("td");
+            var $backgroundTile = document.createElement("td");
+            $foregroundTile.className = "tile";
+            $backgroundTile.className = "tile";
+            $foregroundTile.setAttribute("data-row", String(row));
+            $foregroundTile.setAttribute("data-col", String(col));
+            $backgroundTile.setAttribute("data-row", String(row));
+            $backgroundTile.setAttribute("data-col", String(col));
+            $foregroundTile.addEventListener("mousedown", function () {
+                fromRow = row;
+                fromCol = col;
+            });
+            $foregroundTile.addEventListener("mouseenter", function (e) {
+                if (e.buttons !== 1)
+                    return;
+                toRow = row;
+                toCol = col;
+                game.moveCat(fromRow, fromCol, toRow, toCol);
+                updateTileData($background, $foreground, game);
+                fromRow = toRow = row;
+                fromCol = toCol = col;
+            });
+            $foregroundRow.appendChild($foregroundTile);
+            $backgroundRow.appendChild($backgroundTile);
+        };
+        for (var col = 0; col < game.field.width; col += 1) {
+            _loop_2(col);
+        }
+        $foreground.appendChild($foregroundRow);
+        $background.appendChild($backgroundRow);
+    };
+    for (var row = 0; row < game.field.height; row += 1) {
+        _loop_1(row);
+    }
+    updateTileData($background, $foreground, game);
+});
---
+++ index.ts
@@ -0,0 +1,299 @@
+enum Direction {
+    Right = 0x0,
+    Down = 0x1,
+    Left = 0x2,
+    Up = 0x3,
+}
+
+enum BackgroundType {
+    Floor = 0x0,
+    Exit = 0x1,
+}
+
+enum ForegroundType {
+    Empty = 0x0,
+    Wall = 0x1,
+    CatHead = 0x2,
+    CatBody = 0x3,
+    CatTail = 0x4,
+}
+
+class Tile {
+    constructor(
+        public backgroundType: BackgroundType,
+        public backgroundId: number,
+        public foregroundType: ForegroundType,
+        public foregroundId: number,
+        public headDirection: Direction,
+        public tailDirection : Direction,
+    ) {}
+
+    encode(): number {
+        return (this.backgroundType << 0)
+            | (this.backgroundId << 4)
+            | (this.foregroundType << 8)
+            | (this.foregroundId << 12)
+            | (this.headDirection << 16)
+            | (this.tailDirection << 20);
+    }
+
+    static decode(n: number): Tile {
+        return new Tile(
+            (n >> 0) & 0xf,
+            (n >> 4) & 0xf,
+            (n >> 8) & 0xf,
+            (n >> 12) & 0xf,
+            (n >> 16) & 0xf,
+            (n >> 20) & 0xf,
+        );
+    }
+
+    hasCat(): boolean {
+        return this.foregroundType === ForegroundType.CatHead
+            || this.foregroundType === ForegroundType.CatBody
+            || this.foregroundType === ForegroundType.CatTail;
+    }
+}
+
+class Field {
+    constructor(
+        public width: number,
+        public height: number,
+        public tiles: Tile[][],
+    ) {
+        if (tiles.length !== height) {
+            throw new Error('invalid height');
+        }
+
+        if (tiles.some(row => row.length !== width)) {
+            throw new Error('invalid width');
+        }
+    }
+
+    encode(): Uint32Array {
+        const arr = new Uint32Array(this.height * this.width + 2);
+
+        arr[arr.length - 2] = this.width;
+        arr[arr.length - 1] = this.height;
+
+        for (let row = 0; row < this.height; row += 1) {
+            for (let col = 0; col < this.width; col += 1) {
+                arr[row * this.width + col] = this.tiles[row][col].encode();
+            }
+        }
+
+        return arr;
+    }
+
+    static decode(arr: Uint32Array): Field {
+        const width = arr[arr.length - 2];
+        const height = arr[arr.length - 1];
+
+        if (arr.length !== width * height + 2) {
+            throw new Error('invalid field');
+        }
+
+        const tiles = Array<Array<Tile>>();
+
+        for (let row = 0; row < height; row += 1) {
+            const rowTiles = Array<Tile>();
+
+            for (let col = 0; col < width; col += 1) {
+                rowTiles.push(Tile.decode(arr[row * width + col]));
+            }
+
+            tiles.push(rowTiles);
+        }
+
+        return new Field(width, height, tiles);
+    }
+}
+
+class Game {
+    constructor(
+        readonly field: Field,
+    ) {}
+
+    isWon(): boolean {
+        return this.field.tiles.every(
+            rowTiles => rowTiles.every(
+                tile => tile.hasCat()
+                    && tile.foregroundId === 1
+                    && tile.backgroundType === BackgroundType.Exit
+            )
+        );
+    }
+
+    moveCat(fromRow: number, fromCol: number, toRow: number, toCol: number): void {
+        let fromTile = this.field.tiles[fromRow][fromCol];
+        let toTile = this.field.tiles[toRow][toCol];
+
+        if (
+            fromTile.foregroundType !== ForegroundType.CatHead
+            && fromTile.foregroundType !== ForegroundType.CatTail
+        ) {
+            return;
+        }
+
+        if (toTile.foregroundType !== ForegroundType.Empty) {
+            return;
+        }
+
+        const dist = Math.abs(toRow - fromRow) + Math.abs(toCol - fromCol);
+
+        if (dist !== 1) {
+            return;
+        }
+
+        const otherEndForegroundType =
+            fromTile.foregroundType === ForegroundType.CatHead
+            ? ForegroundType.CatTail
+            : ForegroundType.CatHead;
+        
+        const frontDirection =
+            fromTile.foregroundType === ForegroundType.CatHead
+            ? "headDirection" as const
+            : "tailDirection" as const;
+        
+        const backDirection =
+            fromTile.foregroundType === ForegroundType.CatHead
+            ? "tailDirection" as const
+            : "headDirection" as const;
+        
+        let lastTile = toTile;
+        let lastRow = toRow;
+        let lastCol = toCol;
+
+        while (lastTile.foregroundType !== otherEndForegroundType) {
+            toTile.foregroundId = fromTile.foregroundId;
+            toTile.foregroundType = fromTile.foregroundType;
+
+            if (lastRow - toRow === -1) toTile[frontDirection] = Direction.Up;
+            if (lastRow - toRow === +1) toTile[frontDirection] = Direction.Down;
+            if (lastCol - toCol === -1) toTile[frontDirection] = Direction.Left;
+            if (lastCol - toCol === +1) toTile[frontDirection] = Direction.Right;
+
+            if (fromRow - toRow === -1) toTile[backDirection] = Direction.Up;
+            if (fromRow - toRow === +1) toTile[backDirection] = Direction.Down;
+            if (fromCol - toCol === -1) toTile[backDirection] = Direction.Left;
+            if (fromCol - toCol === +1) toTile[backDirection] = Direction.Right;
+
+            fromTile.foregroundId = 0;
+            fromTile.foregroundType = ForegroundType.Empty;
+
+            lastTile = toTile;
+            lastRow = toRow;
+            lastCol = toCol;
+
+            toTile = fromTile;
+            toRow = fromRow;
+            toCol = fromCol;
+
+            if (fromTile[backDirection] === Direction.Up) fromRow -= 1;
+            if (fromTile[backDirection] === Direction.Down) fromRow += 1;
+            if (fromTile[backDirection] === Direction.Left) fromCol -= 1;
+            if (fromTile[backDirection] === Direction.Right) fromCol += 1;
+
+            fromTile = this.field.tiles[fromRow][fromCol];
+        }
+    }
+}
+
+function setBackgroundTileData(el: Element, tile: Tile): void {
+    el.setAttribute("data-backgroundType", String(tile.backgroundType));
+    el.setAttribute("data-backgroundId", String(tile.backgroundId));
+}
+
+function setForegroundTileData(el: Element, tile: Tile): void {
+    el.setAttribute("data-foregroundType", String(tile.foregroundType));
+    el.setAttribute("data-foregroundId", String(tile.foregroundId));
+    el.setAttribute("data-headDirection", String(tile.headDirection));
+    el.setAttribute("data-tailDirection", String(tile.tailDirection));
+}
+
+function updateTileData($background: Element, $foreground: Element, game: Game): void {
+    $background.querySelectorAll(".tile").forEach($tile => {
+        const row = Number($tile.getAttribute("data-row"));
+        const col = Number($tile.getAttribute("data-col"));
+
+        setBackgroundTileData($tile, game.field.tiles[row][col]);
+    });
+
+    $foreground.querySelectorAll(".tile").forEach($tile => {
+        const row = Number($tile.getAttribute("data-row"));
+        const col = Number($tile.getAttribute("data-col"));
+
+        setForegroundTileData($tile, game.field.tiles[row][col]);
+    });
+}
+
+// const fieldData = Uint32Array.of(
+//     0x000100, 0x000100, 0x000100, 0x000100, 0x000100,
+//     0x000100, 0x131200, 0x312400, 0x000000, 0x000100,
+//     0x000100, 0x131300, 0x312300, 0x000000, 0x000100,
+//     0x000100, 0x131400, 0x312200, 0x000000, 0x000100,
+//     0x000100, 0x000100, 0x000001, 0x000100, 0x000100,
+//     5, 5
+// );
+
+const fieldData = Uint32Array.from(window.location.hash.slice(1).split(',').map(s => Number(s)));
+
+console.log(fieldData.toString());
+
+const game = new Game(Field.decode(fieldData));
+
+let fromRow: number = -1;
+let fromCol: number = -1;
+
+let toRow: number = -1;
+let toCol: number = -1;
+
+window.addEventListener("load", () => {
+    const $foreground = document.getElementById("foreground");
+    const $background = document.getElementById("background");
+
+    for (let row = 0; row < game.field.height; row += 1) {
+        const $foregroundRow = document.createElement("tr");
+        const $backgroundRow = document.createElement("tr");
+
+        for (let col = 0; col < game.field.width; col += 1) {
+            const $foregroundTile = document.createElement("td");
+            const $backgroundTile = document.createElement("td");
+
+            $foregroundTile.className = "tile";
+            $backgroundTile.className = "tile";
+
+            $foregroundTile.setAttribute("data-row", String(row));
+            $foregroundTile.setAttribute("data-col", String(col));
+
+            $backgroundTile.setAttribute("data-row", String(row));
+            $backgroundTile.setAttribute("data-col", String(col));
+
+            $foregroundTile.addEventListener("mousedown", () => {
+                fromRow = row;
+                fromCol = col;
+            });
+
+            $foregroundTile.addEventListener("mouseenter", e => {
+                if (e.buttons !== 1) return;
+
+                toRow = row;
+                toCol = col;
+
+                game.moveCat(fromRow, fromCol, toRow, toCol);
+                updateTileData($background, $foreground, game);
+
+                fromRow = toRow = row;
+                fromCol = toCol = col;
+            });
+
+            $foregroundRow.appendChild($foregroundTile);
+            $backgroundRow.appendChild($backgroundTile);
+        }
+
+        $foreground.appendChild($foregroundRow);
+        $background.appendChild($backgroundRow);
+    }
+
+    updateTileData($background, $foreground, game);
+});
